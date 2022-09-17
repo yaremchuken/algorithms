1406623561a7d2a76d49f7954a7189b1cff9ae4b
yaremchuken <yaremchuken@yandex-team.ru>
2022-09-17 12:37:11 +0300

    Stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,22 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component, FormEvent } from 'react';
+import { Stack } from '../../structs/Stack';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
+import { Input } from '../ui/input/input';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import styles from './stack-page.module.css';
 
 interface StackPageProps {}
 
-interface StackPageState {}
+interface StackPageState {
+  stack: Stack<number>;
+}
 
 class StackPage extends Component<StackPageProps, StackPageState> {
   constructor(props: StackPageProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      stack: new Stack(),
+    };
   }
 
+  onAdd = (e: FormEvent<HTMLButtonElement>) => {
+    const input = +(document.querySelector('#stack-input-field') as HTMLInputElement).value;
+    if (isNaN(input)) return;
+    const { stack } = this.state;
+    stack.push(input);
+    this.setState({
+      stack,
+    });
+  };
+
+  onRemove = () => {
+    const { stack } = this.state;
+    stack.pop();
+    this.setState({
+      stack,
+    });
+  };
+
+  onClear = () => {
+    this.setState({
+      stack: new Stack(),
+    });
+  };
+
   render(): React.ReactNode {
+    const { stack } = this.state;
     return (
       <SolutionLayout title="Стек">
-        <div className={styles.inputBlock}></div>
-        <div className={styles.stackBlock}></div>
+        <div className={styles.inputBlock}>
+          <div className={styles.operationInputs}>
+            <Input maxLength={4} isLimitText id="stack-input-field" />
+            <Button text="Добавить" onClick={this.onAdd} />
+            <Button text="Удалить" onClick={this.onRemove} />
+          </div>
+          <Button text="Очистить" onClick={this.onClear} />
+        </div>
+        <div className={styles.stackBlock}>
+          {stack.map((v, idx) => (
+            <Circle
+              key={idx}
+              letter={`${v}`}
+              index={idx}
+              head={idx === stack.getSize() - 1 ? 'top' : ''}
+            />
+          ))}
+        </div>
       </SolutionLayout>
     );
   }
---
+++ src/structs/Stack.ts
@@ -0,0 +1,29 @@
+interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+  getSize: () => number;
+  map<U>(callbackfn: (value: T, index: number, array: T[]) => U): U[];
+}
+
+export class Stack<T> implements IStack<T> {
+  private container: T[] = [];
+
+  push = (item: T): void => {
+    this.container.push(item);
+  };
+
+  pop = (): void => {
+    this.container.pop();
+  };
+
+  peak = (): T | null => {
+    return this.container[this.container.length - 1];
+  };
+
+  getSize = () => this.container.length;
+
+  map<U>(callbackfn: (value: T, index: number, array: T[]) => U): U[] {
+    return this.container.map(callbackfn);
+  }
+}
